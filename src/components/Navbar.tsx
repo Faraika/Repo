3982243906a3d193dc139c175a0a987d70596e75
Faraika <FaraiChannel@yandex.ru>
2022-03-27 22:53:07 +0300

    Загрузка изначальных файлов

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Navbar: React.FunctionComponent = () => {
+    return <nav>
+        <div className="nav-wrapper light-green darken-3 px1">
+            <a href="#" className="brand-logo">Ежедневник</a>
+        </div>
+
+    </nav>;
+}
